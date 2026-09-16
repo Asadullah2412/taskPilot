52a850bfa73191593db0023190c677b77066989d
Asadullah2412 <asadullahmohammad2412@gmail.com>
2026-09-16 19:43:25 +0300

    smol edit

--- agent.py
+++ agent.py
@@ -20,58 +20,7 @@ agent = create_agent(
 )
 
 
-# def run_agent(question:str):
-#     """Run the agent and print a clean , begineer-friendly execution trace."""
 
-#     print(f"\n🧑 User: {question}")
-#     print("-" * 60)
-
-#     result = agent.ainvoke({
-#         "messages": [("user", question)]
-#     })
-
-#     print("🔎 Clean Agent Execution Trace")
-#     print("-" * 60)
-
-#     step = 1
-#     for msg in result["messages"]:
-
-#         # 1. Human message = original user question
-#         if msg.type == "human":
-#             print(f"{step}. User asked:")
-#             print(f"   {msg.content}")
-#             step += 1
-
-#         # 2. AI message with tool_calls = agent decided to use a tool
-#         elif msg.type == "ai" and getattr(msg, "tool_calls", None):
-#             for tool_call in msg.tool_calls:
-#                 tool_name = tool_call["name"]
-#                 tool_args = tool_call["args"]
-
-#                 print(f"{step}. Agent decision:")
-#                 print(f"   I need to use the tool: {tool_name}")
-#                 print(f"   Tool input: {tool_args}")
-#                 step += 1
-
-#         # 3. Tool message = result returned by the tool
-#         elif msg.type == "tool":
-#             print(f"{step}. Tool observation:")
-#             print(f"   Tool returned: {msg.content}")
-#             step += 1
-
-#         # 4. Final AI message = final response to user
-#         elif msg.type == "ai" and msg.content:
-#             print(f"{step}. Final answer:")
-#             print(f"   {msg.content}")
-#             step += 1
-
-#     print("=" * 60)
-
-
-# run_agent("today i have a buy groceries "
-#           "i have walk 5k steps then excersise too "
-#           "what are my tasks today "
-#           )
 
 import asyncio
 
